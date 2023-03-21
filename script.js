79ed1530b93f254a9cdad1a4856993850e24628d
DesenvolvedorDeGaragem <danrleybrasil.s@gmail.com>
2023-03-20 21:32:34 -0300

    Alterações ultimo período

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
+
+
+function submitWithVariable(nome, profissao) {
+  // alerta o valor do campo
+  alert("Bem vindo " + profissao + " " + nome);
+  // impede o envio do form
+};
+
 function submit() {
   // alerta o valor do campo
-  alert(document.getElementById('name').value);
+  alert("Bem vindo " + document.getElementById('profissao').value + " " + document.getElementById('name').value);
   // impede o envio do form
 };
